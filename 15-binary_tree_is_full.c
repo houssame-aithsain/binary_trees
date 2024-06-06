886e1be0aaf746e726ceed0b6064f7075707f044
Houssame <hossame@inbox.ru>
2024-06-06 17:20:38 +0100

    b

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -18,5 +18,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree->left == NULL || tree->right == NULL)
 		return (0);
 
-	return binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right);
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
