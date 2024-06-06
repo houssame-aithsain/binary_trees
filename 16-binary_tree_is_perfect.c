123fa809cb0bb99ccec72d562c9aa0cf9ac76e12
Houssame <hossame@inbox.ru>
2024-06-06 17:40:52 +0100

    b

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -36,11 +36,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	
+
 	result = tree_is_perfect(tree);
-	
+
 	if (result != 0)
 		return (1);
-	
+
 	return (0);
 }
